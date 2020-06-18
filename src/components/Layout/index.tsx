e4844081f6802fd7939ad3ebfbfa6b52d0916c0e
Daniele <danijovina@gmail.com>
2020-06-18 18:35:41 -0300

    Creating Channel Info component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,12 +4,14 @@ import { Grid } from './styles';
 
 import ServerList from '../ServerList';
 import ServerName from '../ServerName';
+import ChannelInfo from '../ChannelInfo';
 
 const Layout: React.FC = () => {
   return (
     <Grid>
       <ServerList />
       <ServerName />
+      <ChannelInfo />
     </Grid>
   )
 };
