4446557c3e531c1de80200a78e82f772e63ad8cf
Daniele <danijovina@gmail.com>
2020-06-18 18:13:06 -0300

    Creating Server Name component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Grid } from './styles';
 
 import ServerList from '../ServerList';
+import ServerName from '../ServerName';
 
 const Layout: React.FC = () => {
   return (
     <Grid>
       <ServerList />
+      <ServerName />
     </Grid>
   )
 };
---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles';
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>Dani's Server</Title>
+      <ExpandIcon />
+    </Container>
+  )
+};
+
+export default ServerName;
