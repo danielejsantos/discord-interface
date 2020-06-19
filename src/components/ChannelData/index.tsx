d2c10a65f85ec12880ca4561cfa386a9add00872
Daniele <danijovina@gmail.com>
2020-06-19 18:55:41 -0300

    Create Channel Data and Channel Message components

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+import daniele from '../../assets/daniele.png';
+import discord from '../../assets/discord.png';
+import joeyTribbiani from '../../assets/joey-tribbiani.png';
+import yoda from '../../assets/yoda.png';
+import monicaGeller from '../../assets/monica-geller.png';
+import robinScherbatsky from '../../assets/robin-scherbatsky.png';
+import tedMosby from '../../assets/ted-mosby.png';
+import rachelGreen from '../../assets/rachel-green.png';
+import darthVader from '../../assets/darth-vader.png';
+import phoebeBuffay from '../../assets/phoebe-buffay.png';
+
+const ChannelData: React.FC = () => {
+  return (
+    <Container>
+      <Messages>
+        <ChannelMessage
+          author="Discord"
+          date="18/06/2020"
+          content={
+            <>
+              <Mention>@Daniele Santos</Mention>, welcome!
+            </>
+          }
+          hasMention
+          isBot
+          img={discord}
+        />
+        <ChannelMessage
+          author="Daniele Santos"
+          date="19/06/2020"
+          content="Hi guys!"
+          img={daniele}
+        />
+        <ChannelMessage
+          author="Joey Tribbiani"
+          date="19/06/2020"
+          content={
+            <>
+              <Mention>@Daniele Santos</Mention> hey, how you doing?
+            </>
+          }
+          img={joeyTribbiani}
+          hasMention
+        />
+        <ChannelMessage
+          author="Yoda"
+          date="19/06/2020"
+          content="Bored in this quarantine I am"
+          img={yoda}
+        />
+        <ChannelMessage
+          author="Monica Geller"
+          date="19/06/2020"
+          content={
+            <>
+              <Mention>@Yoda</Mention> I KNOW!
+            </>
+          }
+          img={monicaGeller}
+        />
+        <ChannelMessage
+          author="Robin Scherbatsky"
+          date="19/06/2020"
+          content="Let's go to the mall today?"
+          img={robinScherbatsky}
+        />
+        <ChannelMessage
+          author="Ted Mosby"
+          date="19/06/2020"
+          content="I can't. I have a date. And guys, I think she's the one!!!"
+          img={tedMosby}
+        />
+        <ChannelMessage
+          author="Rachel Green"
+          date="19/06/2020"
+          content="I'm in! I have to go shopping anyway!"
+          img={rachelGreen}
+        />
+        <ChannelMessage
+          author="Darth Vader"
+          date="19/06/2020"
+          content="I can't go either. I'm gonna tell my son I'm his father today"
+          img={darthVader}
+        />
+        <ChannelMessage
+          author="Phoebe Buffay"
+          date="19/06/2020"
+          content="I wish I could but I don't want to"
+          img={phoebeBuffay}
+        />
+      </Messages>
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  )
+};
+
+export default ChannelData;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,6 +8,7 @@ import ChannelInfo from '../ChannelInfo';
 import ChannelList from '../ChannelList';
 import UserInfo from '../UserInfo';
 import UserList from '../UserList';
+import ChannelData from '../ChannelData';
 
 const Layout: React.FC = () => {
   return (
@@ -17,7 +18,7 @@ const Layout: React.FC = () => {
       <ChannelInfo />
       <ChannelList />
       <UserInfo />
-      <div />
+      <ChannelData />
       <UserList />
     </Grid>
   )
